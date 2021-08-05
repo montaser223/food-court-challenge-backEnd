b61a795ea9f71c276bfd14441397c8e60be28e46
Montaser <abdelrahmanmontaser12345@gmail.com>
2021-08-05 14:12:57 +0200

    Feat(deploy): add production environment variables

--- config.js
+++ config.js
@@ -4,7 +4,11 @@ const config = {
     PORT: process.env.PORT || 8000,
   },
 
-  prod: {},
+  production: {
+    DB_URI:
+      "mongodb+srv://montaser:0kyjnaJVL9QZKOt0@cluster0.p4sqv.mongodb.net/foodCourt?retryWrites=true&w=majority",
+    PORT: process.env.PORT,
+  },
 };
 
 module.exports = config;
